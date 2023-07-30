7743d0750eb5180fee71bd2c30b0c6138450bc39
Roy, Debakar <debakar.roy@intel.com>
2023-07-30 13:12:12 +0530

    Add unit test

---
+++ content/test_example.py
@@ -0,0 +1,15 @@
+import pytest
+
+@pytest.fixture(autouse=True, scope='session')
+def setup_and_teardown_example():
+    print("Setup - Before the test")
+    yield
+    print("Teardown - After the test")
+
+def test_add():
+    print("Running test_add")
+    assert 1 + 1 == 2
+
+def test_sub():
+    print("Running test_sub")
+    assert 1 - 1 == 0
